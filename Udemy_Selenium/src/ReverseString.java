a52aa03cef6c35644e0e9645197f7bfb427e4b2b
dingGator <kim@chucrew.net>
2017-08-22 15:26:29 -0400

    Reverse_String_Exercise
    Reverse String Exercise

--- Udemy_Selenium/src/ReverseString.java
+++ Udemy_Selenium/src/ReverseString.java
@@ -2,7 +2,39 @@
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String tstr ="this is a test string";
+		
+		
+		System.out.println("before parsing tstr:  " + tstr);
+		String[] parsedTstr = tstr.split(" ");
+		String revTstr =""; 
+		int numWord =0;
+		
+		
+		for(int i=0; i<parsedTstr.length; i++) {
+					
+			System.out.println("after parsed:  " + parsedTstr[i]);
+			numWord =i;
+			System.out.println("i:  " + i);
+			
+		}
+		
+		System.out.println("numWord:  " + numWord);
+		
+		while(numWord > -1) {
+			revTstr = revTstr.concat(parsedTstr[numWord]+" ");
+			System.out.println("revTstr being build:  " + revTstr);
+			
+			numWord--;
+		}
+		
+		System.out.println("revTstr final:  " + revTstr);
+		
+		
+		
+		
+		
+		
 
 	}
 
