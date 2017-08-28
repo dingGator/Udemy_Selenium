8675f0a8c078bf37c9eda77a84815e4eb5630f09
dingGator <kim@chucrew.net>
2017-08-27 23:25:49 -0400

    Inheritance_Exercise
    Inheritance Exercise

---
+++ Udemy_Selenium/src/src/InheritanceDemo.java
@@ -0,0 +1,15 @@
+package src;
+
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		int speed=0;
+		Cars c1 = new Cars(speed);
+		c1.increaseSpeed();
+		
+		BMW bmw1 = new BMW(speed);
+		bmw1.increaseSpeed();
+		bmw1.headupDisplayNavigation();
+	}
+
+}
