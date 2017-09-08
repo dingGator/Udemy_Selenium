32e4ca55df81cf868284b58de9d0ffa58c0abc5a
dingGator <kim@chucrew.net>
2017-09-07 23:05:45 -0400

    Selenium with Eclipse
    Selenium with Eclipse

---
+++ seleniumpractice/src/seleniumpractice/FirstTestClass.java
@@ -0,0 +1,12 @@
+package seleniumpractice;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestClass {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
