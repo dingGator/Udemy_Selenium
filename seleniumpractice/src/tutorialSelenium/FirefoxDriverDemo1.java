dad927447e5ae31d0243eb7f2d762ecdd9a78ff9
dingGator <kim@chucrew.net>
2017-09-09 22:41:15 -0400

    Selenium_Gecko
    Selenium Gecko

---
+++ seleniumpractice/src/tutorialSelenium/FirefoxDriverDemo1.java
@@ -0,0 +1,19 @@
+package tutorialSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxDriverDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		//driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "C://Users//Ki//Documents//GitHub//Udemy_Selenium//Udemy_Selenium//src//geckodriver.exe");
+		
+		
+		//String baseURL = "http://www.google.com";
+		//driver.get(baseURL);
+	}
+
+}
