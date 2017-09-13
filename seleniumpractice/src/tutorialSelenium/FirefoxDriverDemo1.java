5af247f7a4b078eeefa67479dd34e0aa67682af8
dingGator <kim@chucrew.net>
2017-09-12 22:24:57 -0400

    Firefox_Driver Demo
    Firefox Driver installation problem is not resolved

--- seleniumpractice/src/tutorialSelenium/FirefoxDriverDemo1.java
+++ seleniumpractice/src/tutorialSelenium/FirefoxDriverDemo1.java
@@ -1,19 +1,20 @@
 package tutorialSelenium;
 
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirefoxDriverDemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		WebDriver driver;
-		//driver = new FirefoxDriver();
-		System.setProperty("webdriver.gecko.driver", "C://Users//Ki//Documents//GitHub//Udemy_Selenium//Udemy_Selenium//src//geckodriver.exe");
-		
-		
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Ki\\Documents\\GitHub\\Udemy_Selenium\\seleniumpractice\\src\\tutorialSelenium\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		//String baseURL = "http://www.google.com";
-		//driver.get(baseURL);
+		
+		driver.get("http://www.cnn.com");
 	}
 
 }
